097ad5d04e359949e6a74137d998d127055a6f4b
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-08 16:28:05 +0600

    Error page design added

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,19 @@
-
+import { Link } from "react-router-dom";
+import Footer from "../../shared/Footer/Footer";
+import NavBar from "../../shared/NavBar/NavBar";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>Error, Something went wrong</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="container mx-auto">
+        
+        <img className="w-[600px] mx-auto" src="https://i.ibb.co/6YZb523/5203299.jpg"/>
+        <h1 className="text-xl text-center py-4 my-6">Back to the Home <Link className="b-0 p-2 rounded-lg bg-gradient-to-r from-violet-500 to-fuchsia-500 b-0 text-white font-semibold" to={'/'}> Home </Link></h1>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
